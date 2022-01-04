3e66bb5a3b26e33f40a956b1fc9294036547d016
vpereira01 <42812459+vpereira01@users.noreply.github.com>
2022-01-04 12:33:10 +0000

    Added cmd line parsing

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"vscode/gostart/gendata"
 )
 
 func main() {
-	gendata.GenerateRawRecords("./data/gendataCenas", 150, 9)
+	fGenerateRawRecords := flag.Bool("gen", false, "Generate raw records")
+	flag.Parse()
 
-	return
+	if *fGenerateRawRecords {
+		gendata.GenerateRawRecords("./data/gendataCenas", 150, 9)
+	} else {
+		flag.Usage()
+	}
 
 	// easy rsa number 10263280077814176196883978050069
 }
