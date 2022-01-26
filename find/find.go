6d7066cd7209b17cb34ee1cbb3ab93208fd8d729
vpereira01 <42812459+vpereira01@users.noreply.github.com>
2022-01-26 22:19:45 +0000

    Improved find performance greatly

--- find/find.go
+++ find/find.go
@@ -32,19 +32,57 @@ func FindPrimesSub(numberN *big.Int) *big.Int {
 	startNumBigFloat := new(big.Float).SetPrec(bigFloatsDefaultPrecision)
 	fourTimesNumberNBigFloat := new(big.Float).SetInt(fourTimesNumberN)
 	startNumBigFloat.Sqrt(fourTimesNumberNBigFloat)
-	roundDownToPair(startNumBigFloat)
+	roundDownToPairAndNineMultiple(startNumBigFloat)
 	startNum := new(big.Int)
 	startNumBigFloat.Int(startNum)
 
-	twoBigInt := new(big.Int).SetInt64(2)
+	// Let's try to speed up our guess my only generating startNum guesses that produce numbers expected to the perfect squares
+	// We can do this because given fourTimesNumberN % 9 we can now if startNum guesses % 9 values
+	// if fourTimesNumberN % 9 = 0, startNum % 9 must be 0,1,2,3,4,5,6,7,8
+	// if fourTimesNumberN % 9 = 1, startNum % 9 must be 1,8
+	// if fourTimesNumberN % 9 = 2, startNum % 9 must be 0,3,6
+	// if fourTimesNumberN % 9 = 3, startNum % 9 must be 1,2,4,5,7,8
+	// if fourTimesNumberN % 9 = 4, startNum % 9 must be 2,7
+	// if fourTimesNumberN % 9 = 5, startNum % 9 must be 0,3,6
+	// if fourTimesNumberN % 9 = 6, startNum % 9 must be 1,2,4,5,7,8
+	// if fourTimesNumberN % 9 = 7, startNum % 9 must be 4,5
+	// if fourTimesNumberN % 9 = 8, startNum % 9 must be 0,3,6
+	// "Calculated" using https://www.wolframalpha.com/input/?i=solve+%7B+%28x%5E2-4*n%29+mod+9+%3D+0%2C+4*n+mod+9+%3D+8%2C+x%3Cn%2C+x%3E0%2C+n%3E0+%7D+for+x
+	nineModsAndStartIncrements := make(map[uint][]uint)
+	nineModsAndStartIncrements[0] = []uint{0, 10, 2, 12, 4, 14, 6, 16, 8}
+	nineModsAndStartIncrements[1] = []uint{10, 8}
+	nineModsAndStartIncrements[2] = []uint{0, 12, 6}
+	nineModsAndStartIncrements[3] = []uint{10, 2, 4, 14, 16, 8}
+	nineModsAndStartIncrements[4] = []uint{2, 16}
+	nineModsAndStartIncrements[5] = []uint{0, 12, 6}
+	nineModsAndStartIncrements[6] = []uint{10, 2, 4, 14, 16, 8}
+	nineModsAndStartIncrements[7] = []uint{4, 14}
+	nineModsAndStartIncrements[8] = []uint{0, 12, 6}
 
-	return tryFindPrimesSub(fourTimesNumberN, startNum, twoBigInt, bigFloatsDefaultPrecision)
+	nineBigInt := new(big.Int).SetInt64(9)
+	fourTimesNumberNMod9BigInt := new(big.Int)
+	fourTimesNumberNMod9BigInt.Mod(fourTimesNumberN, nineBigInt)
+	fourTimesNumberNMod9 := uint(fourTimesNumberNMod9BigInt.Uint64())
+	eighteenBigInt := new(big.Int).SetInt64(18)
+
+	results := make(chan *big.Int)
+	stopFlag := false
+
+	for _, startIncrement := range nineModsAndStartIncrements[fourTimesNumberNMod9] {
+		startNumAdjusted := new(big.Int)
+		startNumAdjusted.SetUint64(uint64(startIncrement))
+		startNumAdjusted.Add(startNumAdjusted, startNum)
+		go tryFindPrimesSub(fourTimesNumberN, startNumAdjusted, eighteenBigInt, bigFloatsDefaultPrecision, results, &stopFlag)
+	}
+
+	result := <-results
+	stopFlag = true
+	return result
 }
 
 // Tries to find primes subtraction for a given startNum and step
-func tryFindPrimesSub(fourTimesNumberN *big.Int, initialStartNum *big.Int, step *big.Int, bigFloatsPrecision uint) *big.Int {
-	value1Mod9BigInt := new(big.Int)
-	nineBigInt := new(big.Int).SetInt64(9)
+func tryFindPrimesSub(fourTimesNumberN *big.Int, initialStartNum *big.Int, step *big.Int, bigFloatsPrecision uint, results chan *big.Int, stopFlag *bool) {
+	fmt.Printf("fourTimesNumberN: %v initialStartNum: %v step: %v\n", fourTimesNumberN, initialStartNum, step)
 
 	value1Mod100BigInt := new(big.Int)
 	hundredBigInt := new(big.Int).SetInt64(100)
@@ -70,16 +108,13 @@ func tryFindPrimesSub(fourTimesNumberN *big.Int, initialStartNum *big.Int, step 
 		// reference https://mathworld.wolfram.com/SquareNumber.html
 		value1Mod100BigInt.Mod(value1, hundredBigInt)
 		value1Mod100 := value1Mod100BigInt.Int64()
-		value1Mod9BigInt.Mod(value1, nineBigInt)
-		value1Mod9 := value1Mod9BigInt.Int64()
 
-		if (value1Mod9 == 1 || value1Mod9 == 4 || value1Mod9 == 7 || value1Mod9 == 0) &&
-			(value1Mod100 == 0 || value1Mod100 == 1 || value1Mod100 == 4 || value1Mod100 == 9 ||
-				value1Mod100 == 16 || value1Mod100 == 21 || value1Mod100 == 24 || value1Mod100 == 25 ||
-				value1Mod100 == 29 || value1Mod100 == 36 || value1Mod100 == 41 || value1Mod100 == 44 ||
-				value1Mod100 == 49 || value1Mod100 == 56 || value1Mod100 == 61 || value1Mod100 == 64 ||
-				value1Mod100 == 69 || value1Mod100 == 76 || value1Mod100 == 81 || value1Mod100 == 84 ||
-				value1Mod100 == 89 || value1Mod100 == 96) {
+		if value1Mod100 == 0 || value1Mod100 == 1 || value1Mod100 == 4 || value1Mod100 == 9 ||
+			value1Mod100 == 16 || value1Mod100 == 21 || value1Mod100 == 24 || value1Mod100 == 25 ||
+			value1Mod100 == 29 || value1Mod100 == 36 || value1Mod100 == 41 || value1Mod100 == 44 ||
+			value1Mod100 == 49 || value1Mod100 == 56 || value1Mod100 == 61 || value1Mod100 == 64 ||
+			value1Mod100 == 69 || value1Mod100 == 76 || value1Mod100 == 81 || value1Mod100 == 84 ||
+			value1Mod100 == 89 || value1Mod100 == 96 {
 
 			countPerfectSquareEstimated++
 
@@ -90,7 +125,7 @@ func tryFindPrimesSub(fourTimesNumberN *big.Int, initialStartNum *big.Int, step 
 				subEst := new(big.Int)
 				subEstBigFloat.Int(subEst)
 				fmt.Printf("-----> subEst: %10v countSteps: %10v countPerfectSquareEstimated: %10v\n", subEst, countSteps, countPerfectSquareEstimated)
-				return subEst
+				results <- subEst
 			}
 		}
 
@@ -98,29 +133,39 @@ func tryFindPrimesSub(fourTimesNumberN *big.Int, initialStartNum *big.Int, step 
 		countSteps++
 
 		if countSteps >= math.MaxUint32 {
-			panic("failed to find result")
+			fmt.Printf("stoping due math.MaxUint32\n")
+			break
+		}
+
+		if *stopFlag {
+			break
 		}
 	}
 }
 
-// Sets bigFloat to its floor value and then subtract one if odd
 // warning: changes precision
-func roundDownToPair(bigFloat *big.Float) {
+func roundDownToPairAndNineMultiple(bigFloat *big.Float) {
 	// if already rounded, it's an int, nothing to do
 	if bigFloat.IsInt() {
 		return
 	}
 
+	// convert to big.Int
 	tempBigInt := new(big.Int)
-
 	bigFloat.Int(tempBigInt)
-	oneBigInt := new(big.Int).SetInt64(1)
-	twoBigInt := new(big.Int).SetInt64(2)
 
+	// subtract until multiple of nine
+	nineBigInt := new(big.Int).SetInt64(9)
 	modBigInt := new(big.Int)
+	modBigInt.Mod(tempBigInt, nineBigInt)
+	tempBigInt.Sub(tempBigInt, modBigInt)
+
+	// if not even, reduce more 9 units
+	twoBigInt := new(big.Int).SetInt64(2)
 	modBigInt.Mod(tempBigInt, twoBigInt)
 	if modBigInt.Int64() == 1 {
-		tempBigInt.Sub(tempBigInt, oneBigInt)
+		tempBigInt.Sub(tempBigInt, nineBigInt)
 	}
+
 	bigFloat.SetInt(tempBigInt)
 }
--- find/find_test.go
+++ find/find_test.go
@@ -15,15 +15,17 @@ func getBigInt(bigIntAsString string) *big.Int {
 }
 
 var findPrimesSubTests = []findPrimesSubTest{
-	{getBigInt("13322573880505234223"), getBigInt("25384478")},
-	{getBigInt("13065662271585108503"), getBigInt("427464298")},
-	{getBigInt("13940064823464665839"), getBigInt("1039934370")},
-	{getBigInt("14042965475976382991"), getBigInt("656435330")},
-	{getBigInt("16648983298230638723"), getBigInt("80336162")},
-	{getBigInt("13797531713940771079"), getBigInt("1008271722")},
-	{getBigInt("13782080400846584441"), getBigInt("33299240")},
-	{getBigInt("13489600177963835063"), getBigInt("287625418")},
-	{getBigInt("13257863350502626553"), getBigInt("517219168")},
+	{getBigInt("13322573880505234223"), getBigInt("25384478")},   // 4*n % 9 = 0
+	{getBigInt("13065662271585108503"), getBigInt("427464298")},  // 4*n % 9 = 1
+	{getBigInt("13371152238833765231"), getBigInt("667716974")},  // 4*n % 9 = 2
+	{getBigInt("16648983298230638723"), getBigInt("80336162")},   // 4*n % 9 = 3
+	{getBigInt("14330847798237362867"), getBigInt("613730734")},  // 4*n % 9 = 4
+	{getBigInt("13797531713940771079"), getBigInt("1008271722")}, // 4*n % 9 = 5
+	{getBigInt("13940064823464665839"), getBigInt("1039934370")}, // 4*n % 9 = 6
+	{getBigInt("14042965475976382991"), getBigInt("656435330")},  // 4*n % 9 = 6
+	{getBigInt("13257863350502626553"), getBigInt("517219168")},  // 4*n % 9 = 6
+	{getBigInt("13489600177963835063"), getBigInt("287625418")},  // 4*n % 9 = 7
+	{getBigInt("13782080400846584441"), getBigInt("33299240")},   // 4*n % 9 = 8
 }
 
 func TestFindPrimesSub(t *testing.T) {
